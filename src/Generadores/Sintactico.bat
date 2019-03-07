d8715e898af6ce83018cfe2e9da8ca7967712abe
andre <andre@192.168.1.15>
2019-03-07 13:13:52 -0600

    SEXTO Comit

--- src/Generadores/Sintactico.bat
+++ src/Generadores/Sintactico.bat
@@ -1,6 +1,6 @@
 SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_05\bin"
 SET PATH=%JAVA_HOME%;%PATH%
 SET CLASSPATH=%JAVA_HOME%;
-cd C:\Users\andre\OneDrive\fotos\Documents\NetBeansProjects\Tarea#3_OLC\src\Analizadores
+cd C:\Users\andre\OneDrive\fotos\Documents\NetBeansProjects\Tarea#3_OLC1\src\Analizadores
 java -jar C:\Users\andre\Desktop\Fuentes\java-cup-11b.jar -parser analisis_sintactico -symbols Simbolos A_Sintactico.cup
 pause
